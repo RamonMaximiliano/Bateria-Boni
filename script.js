b09c05beb9ad8c3d80ebf2b08c9e0cb44d5605f1
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2021-12-01 23:01:52 -0300

    Style finished

---
+++ script.js
@@ -0,0 +1,92 @@
+function styleW() {
+
+    document.getElementById("W").classList.remove("letter")
+    document.getElementById("W").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("W").classList.remove("buttonHover")
+        document.getElementById("W").classList.add("letter")
+    }
+}
+function styleQ() {
+
+    document.getElementById("Q").classList.remove("letter")
+    document.getElementById("Q").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("Q").classList.remove("buttonHover")
+        document.getElementById("Q").classList.add("letter")
+    }
+}
+function styleE() {
+
+    document.getElementById("E").classList.remove("letter")
+    document.getElementById("E").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("E").classList.remove("buttonHover")
+        document.getElementById("E").classList.add("letter")
+    }
+}
+function styleA() {
+
+    document.getElementById("A").classList.remove("letter")
+    document.getElementById("A").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("A").classList.remove("buttonHover")
+        document.getElementById("A").classList.add("letter")
+    }
+}
+function styleS() {
+
+    document.getElementById("S").classList.remove("letter")
+    document.getElementById("S").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("S").classList.remove("buttonHover")
+        document.getElementById("S").classList.add("letter")
+    }
+}
+function styleD() {
+
+    document.getElementById("D").classList.remove("letter")
+    document.getElementById("D").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("D").classList.remove("buttonHover")
+        document.getElementById("D").classList.add("letter")
+    }
+}
+function styleZ() {
+
+    document.getElementById("Z").classList.remove("letter")
+    document.getElementById("Z").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("Z").classList.remove("buttonHover")
+        document.getElementById("Z").classList.add("letter")
+    }
+}
+function styleX() {
+
+    document.getElementById("X").classList.remove("letter")
+    document.getElementById("X").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("X").classList.remove("buttonHover")
+        document.getElementById("X").classList.add("letter")
+    }
+}
+function styleC() {
+
+    document.getElementById("C").classList.remove("letter")
+    document.getElementById("C").classList.add("buttonHover")
+    setTimeout(backNormal, 100);
+    function backNormal(){
+        document.getElementById("C").classList.remove("buttonHover")
+        document.getElementById("C").classList.add("letter")
+    }
+}
+
+
